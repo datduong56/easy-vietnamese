42ebe2621908c4338d77b4bf93ac02e121ed3e85
datduong56 <minicatdd7@gmail.com>
2021-03-18 22:54:02 +0700

    feat: init project 🎉🚀

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigators': './src/navigators',
+          '@i18n': './src/i18n',
+          '@stores': './src/stores',
+          '@services': './src/services',
+          '@core': './src/core',
+        },
+      },
+    ],
+  ],
+};
