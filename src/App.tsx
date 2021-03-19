5e126157c404752525f3d16225303fabd8dd087c
datduong56 <minicatdd7@gmail.com>
2021-03-19 23:39:29 +0700

    feat: add react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {} from 'react-native';
 import Home from '@screens/home';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+  );
 };
 
 export default App;
